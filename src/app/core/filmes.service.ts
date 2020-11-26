4f918e17f2274a4bcb78ad6e6219558174248985
Naira Magalhães <nairadanile@gmail.com>
2020-11-26 00:05:55 -0300

    - colocado scroll infinito, arrumei a ordenação dos filmes e colocado busca por nomes e generos.

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ConfigParams } from '../shared/models/config-params';
 import { Filme } from '../shared/models/filme';
+import { ConfigParamsService } from './config-params.service';
 
 const url = 'http://localhost:3000/filmes/';
 
@@ -10,10 +12,17 @@ const url = 'http://localhost:3000/filmes/';
 })
 export class FilmesService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private configService: ConfigParamsService ) { }
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
+  }
+
+  listar(config: ConfigParams): Observable<Filme[]>{
+    const configParams = this.configService.configurarParametros(config);
+
+    return this.http.get<Filme[]>(url, {params: configParams});
   }
 
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators'
+
+import { FilmesService } from 'src/app/core/filmes.service';
+import { ConfigParams } from 'src/app/shared/models/config-params';
+import { Filme } from 'src/app/shared/models/filme';
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -7,13 +13,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  constructor() { }
+    readonly semFoto = 'https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg';
 
-  ngOnInit() {
+    config: ConfigParams = {
+    pagina: 0,
+    limite: 4,
 
+  };
+  filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
+  
+  
+
+  constructor(private filmesService: FilmesService,
+                      private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges.pipe(debounceTime(400)).subscribe((val: string) => {
+      this.config.pesquisa= val;
+      this.resetarConsulta();
+    })
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.config.campo= {tipo: 'genero', valor: val};
+      this.resetarConsulta();
+    })
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção Cientifica', 'Comédia', 'Drama'];
+
+    this.listarFilmes();
   }
 
-  open() {
+  onScroll(): void{
+    this.listarFilmes();
+  }
+
+  private listarFilmes(): void{
+    this.config.pagina++;
+    this.filmesService.listar(this.config).
+    subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void{
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 
 }
